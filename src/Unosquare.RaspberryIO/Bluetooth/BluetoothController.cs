3967393d8a0d8db2fa3e9ab5d0b86ae72cdd0b83
Alvaro Padilla <alvaro.padilla@unosquare.com>
2017-03-07 08:55:14 -0600

    Add files via upload

---
+++ src/Unosquare.RaspberryIO/Bluetooth/BluetoothController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unosquare.Swan.Abstractions;
+using Unosquare.Swan.Components;
+
+namespace Unosquare.RaspberryIO.Bluetooth
+{
+    public class BluetoothController : SingletonBase<BluetoothController>
+    {
+        #region SimpleFunctionBluetooth
+
+        /// <summary>
+        /// Retrieves current bluetooth status.
+        /// </summary>
+        public string GetStatusBT() => ProcessRunner.GetProcessOutputAsync("systemctl", "status bluetooth").Result;
+
+        //public string InitializeBT() => ProcessRunner.GetProcessOutputAsync("sudo", "bluetoothctl").Result;
+
+        #endregion
+    }
+}
